099186a74248f2525e753c6851ee5d5fa95d0f51
Sean <sd.cotter45@gmail.com>
2022-12-05 05:43:13 -0500

    added some seo and image sizes

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -51,33 +51,68 @@ export default function Footer() {
         >
           Copyright © 2022 The Yardist Lawn Services - All Rights Reserved.
         </h3>
-        <picture>
-          <source srcSet={cutGrassP} type="image/webp" />
-          <source srcSet={cutGrass} type="image/jpeg" />
-          <img
-            src={grass}
-            alt="Grass"
-            style={{
-              filter: "hue-rotate(-25deg) brightness(1)",
-            }}
-            className="w-full h-full max-h-[300px] fixed bottom-0 left-0 z-10 object-fill"
-          />
-        </picture>
-        <picture className="max-h-[100px] overflow-hidden">
-          <source srcSet={grassP} type="image/webp" />
-          <source srcSet={grass} type="image/jpeg" />
-          <img
-            src={grass}
-            alt="Grass"
-            style={{
-              bottom: totalScroll
-                ? `${Math.floor((scrollY / totalScroll) * 100) - 175}px`
-                : "-175px",
-              filter: "hue-rotate(-20deg) saturate(1.3) brightness(.7)",
-            }}
-            className="w-full h-full max-h-[300px] fixed bottom-0 left-0 z-10 object-fill"
-          />
-        </picture>
+        {window && window.innerWidth > 900 && (
+          <>
+            <picture>
+              <source srcSet="./cutGrassLg.webp" type="image/webp" />
+              <source srcSet="./cutGrassLg.png" type="image/jpeg" />
+              <img
+                src="./cutGrassLg.png"
+                alt="Grass"
+                style={{
+                  filter: "hue-rotate(-25deg) brightness(1)",
+                }}
+                className="w-full h-full max-h-[300px] fixed bottom-0 left-0 z-10 object-fill"
+              />
+            </picture>
+            <picture className="max-h-[100px] overflow-hidden">
+              <source srcSet="./grassLg.webp" type="image/jpeg" />
+              <source srcSet="./grassLg.png" type="image/webp" />
+              <img
+                src="./grassLg.png"
+                alt="Grass"
+                style={{
+                  bottom: totalScroll
+                    ? `${Math.floor((scrollY / totalScroll) * 100) - 175}px`
+                    : "-175px",
+                  filter: "hue-rotate(-20deg) saturate(1.3) brightness(.7)",
+                }}
+                className="w-full h-full max-h-[300px] fixed bottom-0 left-0 z-10 object-fill"
+              />
+            </picture>
+          </>
+        )}
+        {window && window.innerWidth < 900 && (
+          <>
+            <picture>
+              <source srcSet={cutGrassP} type="image/webp" />
+              <source srcSet={cutGrass} type="image/jpeg" />
+              <img
+                src={grass}
+                alt="Grass"
+                style={{
+                  filter: "hue-rotate(-25deg) brightness(1)",
+                }}
+                className="w-full h-full max-h-[300px] fixed bottom-0 left-0 z-10 object-fill"
+              />
+            </picture>
+            <picture className="max-h-[100px] overflow-hidden">
+              <source srcSet={grassP} type="image/webp" />
+              <source srcSet={grass} type="image/jpeg" />
+              <img
+                src={grass}
+                alt="Grass"
+                style={{
+                  bottom: totalScroll
+                    ? `${Math.floor((scrollY / totalScroll) * 100) - 175}px`
+                    : "-175px",
+                  filter: "hue-rotate(-20deg) saturate(1.3) brightness(.7)",
+                }}
+                className="w-full h-full max-h-[300px] fixed bottom-0 left-0 z-10 object-fill"
+              />
+            </picture>
+          </>
+        )}
       </div>
     );
   }
--- app/root.tsx
+++ app/root.tsx
@@ -15,6 +15,13 @@ export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "The Yardist Lawn Services",
   viewport: "width=device-width,initial-scale=1",
+  name: "The Yardist Lawn Services",
+  description:
+    "We handle all season maintenance including lawn, leaf, snow and ice in Montgomery, Delaware, Bucks, Berks and Chester counties.",
+  keywords:
+    "pennsylvania, pa, montgomery, delaware, bucks, berks, chester, service, stuck, lawn, grass, mow, mowing, trim, trimming, edge, edging, landscape, landscaping, clean, clean-up, rake, raking, blow, blowing, mulch, mulching, leaf, leaves, snow, snowing, plow, plowing, shovel, shoveling, salt, salting, spread, spreading, sidewalk, ice, spring, fall, winter, summer, top, top-rated, satisfaction, customers",
+  robots: "index, follow",
+  language: "en",
 });
 
 export function links() {
